3625fd4fbc0203fa31803e9a77210f32ca249016
Rosario Nici <rosarionici95@gmail.com>
2023-04-18 16:00:34 +0200

    add link

--- src/app/works/works-cards.ts
+++ src/app/works/works-cards.ts
@@ -4,21 +4,24 @@ export const cards = [
     description:'Riproduzione delle famosissima Web App per Messaggistica',
     skills: [],
     buttons: [],
-    img:'../../assets/img/bloozap.png'
+    img:'../../assets/img/bloozap.png',
+    link: 'https://github.com/RosarioNici/vue-boolzapp'
   },
   {
     title: 'Gli zii di Sicilia',
     description:'Sviluppo sito vetrina per piccola azienda Territoriale siciliana',
     skills: [],
     buttons: [],
-    img:'../../assets/img/Glizii.png'
+    img:'../../assets/img/Glizii.png',
+    link: 'https://gliziidisicilia.com/'
   },
   {
     title: 'Responsive',
     description:'Esercizio mirato per il responsive con esclusivo utilizzo di bootstrap',
     skills: [],
     buttons: [],
-    img:'../../assets/img/responsive-bootstrap.png'
+    img:'../../assets/img/responsive-bootstrap.png',
+    link: 'https://github.com/RosarioNici/html-css-bootstrap-freelancer'
   },
 
   {
@@ -26,20 +29,23 @@ export const cards = [
     description:'Riproduzione delle famosissima Web App per Riproduzioni multimediali di brani musicali',
     skills: [],
     buttons: [],
-    img:'../../assets/img/Shopyfi-web.png'
+    img:'../../assets/img/Shopyfi-web.png',
+    link: 'https://github.com/RosarioNici/html-css-spotifyweb'
   },
    {
     title: 'SEQA.it',
     description:'Sviluppo sito web per la mia vecchia azienda alla quale ho sviluppato diverse interfacce',
     skills: [],
     buttons: [],
-    img:'../../assets/img/Seqa.png'
+    img:'../../assets/img/Seqa.png',
+    link: 'https://seqa.it/'
   },
    {
     title: 'Playstation static web site',
     description:'Sviluppo statico della intera homepage del sito di Sony Playstation',
     skills: [],
     buttons: [],
-    img:'../../assets/img/play.png'
+    img:'../../assets/img/play.png',
+    link: 'https://github.com/RosarioNici/htmlcss-playstation'
   },
 ]
