e7aa671f95ed40ca1ae1611af9b2a622a4f24e84
Rosario Nici <rosarionici95@gmail.com>
2023-04-24 09:46:27 +0200

    correction skills

--- src/app/works/works-cards.ts
+++ src/app/works/works-cards.ts
@@ -2,7 +2,7 @@ export const cards = [
   {
     title: 'Bloozaap',
     description:'Riproduzione delle famosissima Web App per Messaggistica',
-    skills: ['Html', 'CSS', 'Bootstrap', 'Js'],
+    skills: 'Html - CSS - Bootstrap - Js',
     buttons: [],
     img:'../../assets/img/bloozap.png',
     link: 'https://github.com/RosarioNici/vue-boolzapp'
@@ -10,7 +10,7 @@ export const cards = [
   {
     title: 'Gli zii di Sicilia',
     description:'Sviluppo sito vetrina per piccola azienda Territoriale siciliana',
-    skills: ['Angular', 'Firebase', 'Html', 'SASS', 'Ts', 'Bootstrap'],
+    skills: 'Angular - Firebase - Html - SASS- Ts - Bootstrap',
     buttons: [],
     img:'../../assets/img/Glizii.png',
     link: 'https://gliziidisicilia.com/'
@@ -18,7 +18,7 @@ export const cards = [
   {
     title: 'Responsive',
     description:'Esercizio mirato per il responsive con esclusivo utilizzo di bootstrap',
-    skills: ['Html', 'CSS', 'Bootstrap'],
+    skills: 'Html - CSS - Bootstrap',
     buttons: [],
     img:'../../assets/img/responsive-bootstrap.png',
     link: 'https://github.com/RosarioNici/html-css-bootstrap-freelancer'
@@ -27,7 +27,7 @@ export const cards = [
   {
     title: 'Spotify',
     description:'Riproduzione delle famosissima Web App per Riproduzioni multimediali di brani musicali',
-    skills: ['Html', 'CSS', 'Bootstrap'],
+    skills: 'Html - CSS - Bootstrap',
     buttons: [],
     img:'../../assets/img/Shopyfi-web.png',
     link: 'https://github.com/RosarioNici/html-css-spotifyweb'
@@ -35,15 +35,15 @@ export const cards = [
    {
     title: 'SEQA.it',
     description:'Sviluppo sito web per la mia vecchia azienda alla quale ho sviluppato diverse interfacce',
-    skills: ['Angular', 'Firebase', 'Html', 'SASS', 'Ts', 'Bootstrap'],
+    skills: 'Angular - Firebase - Html - SASS - Ts - Bootstrap',
     buttons: [],
     img:'../../assets/img/Seqa.png',
     link: 'https://seqa.it/'
   },
    {
-    title: 'Playstation static web site',
+    title: 'Playstation web site',
     description:'Sviluppo statico della intera homepage del sito di Sony Playstation',
-    skills: ['Html', 'CSS', 'Bootstrap'],
+    skills: 'Html - CSS - Bootstrap',
     buttons: [],
     img:'../../assets/img/play.png',
     link: 'https://github.com/RosarioNici/htmlcss-playstation'
